c21f609133be7e1082985ed544d673cac4db935a
vidur2 <vmod2005@gmail.com>
2024-02-11 06:52:00 -0500

    things

--- pages/api/get_model_val.js
+++ pages/api/get_model_val.js
@@ -1,5 +1,28 @@
+import { MongoClient } from "mongodb";
+
+const dimMapping = {
+    "qb": 5,
+    "rb": 6,
+    "wr": 6,
+    "def": 6
+}
+
+const posMapping = {
+    "qb": "qbr",
+    "rb": "rr",
+    "wr": "rr",
+    "def": "dr"
+}
+
+const colMappping = {
+    "qb": "qb",
+    "def": "def",
+    "rb": "rb/wr",
+    "wr": "rb/wr"
+}
+
 export default async function handler(req, res) {
-    const { name, position } = JSON.parse(req);
+    const { name, position } = req.body;
     const client = new MongoClient("mongodb+srv://root:YvvJtMwFiFJRC0OU@football-player-mapping.tke63u1.mongodb.net/?retryWrites=true&w=majority");
     await client.connect();
     const database = client.db("football-player-mappings");
@@ -10,12 +33,12 @@ export default async function handler(req, res) {
         position: position
     }
 
-    const res = await fetch("http://localhost:5000/predict", {
+    const apiRes = await fetch("http://localhost:5001/predict", {
         method: "POST",
         body: JSON.stringify(body)
     })
 
-    const out = await res.json();
-
-    return out
+    const out = await apiRes.json();
+    console.log(out);
+    return res.status(200).json(out)
 }
