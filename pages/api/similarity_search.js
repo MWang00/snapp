2ecc12c13a9fb6e5e202ed601396bf68f0b4386f
vidur2 <vmod2005@gmail.com>
2024-02-10 21:51:02 -0500

    tmp

--- pages/api/similarity_search.js
+++ pages/api/similarity_search.js
@@ -1,9 +1,53 @@
 // const url = 'https://api.sampleapis.com/wines/reds';
+import { MongoClient } from "mongodb";
 
 
 export default async function handler(req, res) {
-    const { playerName, playerStats } = JSON.parse(req.body);
+    const { playerName, playerStats, position } = JSON.parse(req.body);
     const embeddingVector = avgPlayer(playerStats);
+    const client = MongoClient("mongodb+srv://root:YvvJtMwFiFJRC0OU@football-player-mapping.tke63u1.mongodb.net/?retryWrites=true&w=majority");
+    await client.connect();
+    const database = client.db("football-player-mappings");
+    let col;
+    switch (position) {
+        case "qb":
+            col = database.collection("qb");
+            break;
+        case "wr" | "rb":
+            col = database.collection("rb/wr");
+            break;
+        default:
+            col = database.collection("def");
+    }
+    let agg;
+
+    if (position === "wr" || position === "rb")  {
+        agg = [
+            {
+                '$vectorSearch': {
+                    'index': 'vector_index',
+                    'filter': {
+                        'position': {
+                            '$eq': [position]
+                        }
+                    }
+                },
+                "queryVector": embeddingVector,
+                "limit": 5
+            }
+        ];
+    } else {
+        agg = [
+            {
+                "$vectorSearch": {
+                    "queryVector": embeddingVector,
+                    "limit": 5
+                }
+            }
+        ]
+    }
+
+    const result = await col.aggregate(agg);
 }
 
 function avgPlayer(player) {
